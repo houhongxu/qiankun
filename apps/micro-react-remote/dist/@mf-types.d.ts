a66a0737bdefc47222ea8e3afe21faf2911cade2
houhongxu <hongxu.hou@goodwe.com>
2025-02-14 17:08:27 +0800

    chore: 移除dist

--- apps/micro-react-remote/dist/@mf-types.d.ts
+++
@@ -1,3 +0,0 @@
-
-    export type RemoteKeys = 'REMOTE_ALIAS_IDENTIFIER/Button';
-    type PackageType<T> = T extends 'REMOTE_ALIAS_IDENTIFIER/Button' ? typeof import('REMOTE_ALIAS_IDENTIFIER/Button') :any;
